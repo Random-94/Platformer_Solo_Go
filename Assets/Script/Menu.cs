64f6470e16e795520ce6e692ec385a2686ead993
Randoom <m.bachich@etudiant.ecolemelies.fr>
2020-10-21 10:21:17 +0200

    Ajout Menu

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void playBouton()
+    {
+        SceneManager.LoadScene("SampleScene");//Charge la scène du jeu
+    }
+
+    public void creditsBouton()
+    {
+        SceneManager.LoadScene("MyCredits");//Charge la scène de crédits
+    }
+
+    //Fonction pour le bouton "Quitter"
+    public void quitBouton()
+    {
+        Debug.Log("Ferme le jeu");//Code de débug pour voir si le bouton réagit bien.
+        Application.Quit();//Ferme et Arrête l'application
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
